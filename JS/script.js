5cfd983e06cb02b0579323a5897992e6cbc06783
MilliBill <deme23ke@student.ju.se>
2023-12-01 11:38:27 +0100

    small changes

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,14 @@
-
-
-lightbox.addEventListener ('click', e=> {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove ('active')
-})
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+});
 
 function openLightbox(imageSrc, caption) {
-    const lightbox = document.getElementById('lightbox');
-    const lightboxImage = document.getElementById('lightbox-image');
-    const lightboxCaption = document.getElementById('lightbox-caption');
-  
-    lightboxImage.src = imageSrc;
-    lightboxCaption.textContent = caption;
-  
-    lightbox.style.display = 'flex';
-  }
-  
-  function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
-  }
+  const lightbox = document.getElementById("lightbox");
+
+  lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
